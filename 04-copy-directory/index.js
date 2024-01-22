85fc7a80074cc41ac14830d7fcaf40380be57deb
Kirill Kornilov <k-korni1ov@yandex.ru>
2024-01-22 22:40:47 +0400

    feat: add 04task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+const checkAndRMcopy = async () => {
+  console.log('checkAndRMcopy');
+  await fsPromises.rm(path.join(__dirname, 'files-copy'), {
+    recursive: true,
+  });
+};
+
+const createFold = async () => {
+  console.log('CreateDirec');
+  try {
+    await fsPromises.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      //  console.log('Directory created successfully!');
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const readir = async () => {
+  console.log('ReadDir');
+  const files = await fsPromises.readdir(path.join(__dirname, '/files'), {
+    withFileTypes: true,
+  });
+
+  console.log(files);
+
+  files.forEach((file) => {
+    //  console.log(files);
+    fs.copyFile(
+      `${path.join(__dirname, 'files', file['name'])}`,
+      `${path.join(__dirname, 'files-copy', file['name'])}`,
+      (err) => {
+        if (err) {
+          console.log('Error Found:', err);
+        }
+      },
+    );
+  });
+};
+
+const main = async () => {
+  await checkAndRMcopy();
+  await createFold();
+  await readir();
+};
+
+main();
